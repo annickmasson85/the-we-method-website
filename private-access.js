d8aa5dc7d53abe3a5215b5814a1aceafd58f50cb
annickmasson85 <annickmasson85@gmail.com>
2026-09-22 10:57:51 -0500

    Fix Private Access footer and activate At a Glance

--- private-access.js
+++ private-access.js
@@ -1,4 +1,2 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("member-first-name").textContent = "NOELY";
-  document.getElementById("member-profile-name").textContent = "Noely Mercier";
 });
